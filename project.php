be2a18f71235d9eabfa6d8a315414b6f0241b55d
alsongard <alsongard@gmail.com>
2024-02-04 19:00:17 -0500

    headerProjects

---
+++ project.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width initial-scale=1.0">
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<main>
+			<!-------------HEADER------------------------>
+			<include once="header.php"></include>
+			<!-------------HEADER------------------------>
+
+			<section>
+				<h1>PROJECTS</h1>
+			</section>
+		</main>
+
+	</body>
+</html>
