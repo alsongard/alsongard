447de51828c921689806e1e1fd20faf8f27d4932
alsongard <alsongard@gmail.com>
2024-11-05 22:27:27 -0500

    footer component

---
+++ alsongard/src/components/footer.js
@@ -0,0 +1,54 @@
+function Footer(){
+    return (
+        <div>
+            <footer id="contact">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-col">
+                            <h4>MyPortfolio</h4>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    <div class="footer-col">
+                        <h4>Get In Touch</h4>
+                        <ul>
+                            <li>Email: alsongard@gmail.com</li>
+                            <li>Telephone: +254740046951</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Services</h4>
+                        <ul>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Network Device Configuration</a></li>
+                            <li><a href="#">Software Development</a></li>
+                            <li><a href="#">Penetration Tester</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Follow Me</h4>
+                        <div class="social-links">
+                            <a href="#"><i class='bx bxl-instagram'></i></a>
+                            <a href="https://github.com/alsongard"><i class='bx bxl-github'></i></a>
+                            <a href="alsongard@gmail.com"><i class='bx bxl-gmail'></i></a>
+                            <a href="alsong@alsongadizo"><i class='bx bxl-twitter'></i></a>
+                        </div>  
+                    </div>
+                </div>
+                
+            </div>
+            <div class="lastElement">
+                <p>Made with <span><i class='bx bx-laugh'></i></span> by Alson Gard </p>
+            </div>
+            </footer>
+        </div>
+        
+    )
+}
