704b8c7a5271e3764eec6441ea13073425d02106
alsongard <alsongard@gmail.com>
2025-01-30 14:04:56 +0200

    project Data

---
+++ devportfolio/src/components/projectData.js
@@ -0,0 +1,61 @@
+const data = {
+    "web_dev_projects": [
+        {
+            id: 1,
+            name : "rapid_games_webapp", 
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum",
+            url: "Lorem Ispum"
+
+        },
+        {
+            id: 2,
+            name : "architecture webapp",
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum",
+            url: "Lorem Ispum"
+        },
+        {
+            id: 3,
+            name : "michelles restaurant",
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum", 
+            url: "Lorem Ispum"
+        },
+        {
+            id: 4,
+            name : "crime report website restaurant",
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum",
+            url: "Lorem Ispum"
+
+        }
+    ],
+    "data_science_projects":
+    [
+        {
+            id: 1,
+            name: "air pollution prediction model",
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum",
+            url: "Lorem Ispum"
+
+        },
+        {
+            id: 2,
+            name: "yts.mx web scrapper", 
+            short_description:"Lorem Ispum",
+            description: "Lorem Ispum",
+            img: "Lorem Ispum",
+            url: "Lorem Ispum"
+        }
+    ]
+
+}
+
+export default data;
