0b4794c25f030345b77644e7a6d5a84ff93f5c98
alsongard <alsongard@gmail.com>
2025-01-30 14:07:08 +0200

    projectPage for project route

--- devportfolio/src/components/ProjectPage.js
+++ devportfolio/src/components/ProjectPage.js
@@ -1,12 +1,20 @@
-// import ProjectComponentView from "./projectView";
+import ProjectCategoryView from "./projectCategoryView";
 import data from "./projectData";
 function ProjectPage()
 {
-    const myElement = Object.keys(data);
-    console.log(myElement)
+    const projectTypes = Object.keys(data);
+    console.log(projectTypes)
     return (
-        <section>
-            <h1>Project Page</h1>
+        <section className="mt-[20px]">
+            <h1 className="text-center text-[23px]">Project Page</h1>
+            <h2 className="text-center">Where will and intelligence is crafted beyond.</h2>
+            <div className="w-[80%] mt-[30px] bg-red-300 mx-auto">
+                {
+                    Object.keys(data).map((projectType)=>(
+                        <ProjectCategoryView key={projectType} projectCategoryName={projectType} projects={data[projectType]}/>
+                    ))
+                }
+            </div>
         </section>
     )
 }
