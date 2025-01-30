a3c15e4d6b9305f41025a37f515f59c1aafa3285
alsongard <alsongard@gmail.com>
2025-01-30 14:30:15 +0200

    update on routes and components

--- devportfolio/src/components/mains.js
+++ devportfolio/src/components/mains.js
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./header";
 import Home from "./home";
 import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom"
+import Services from "./service";
+import ProjectPage from "./ProjectPage";
+import Contact from "./contact";
 function Page()
 {
     const [darkMode, setDarkMode] = React.useState(false);
@@ -12,8 +15,9 @@ function Page()
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<div><Header darkModeNew={darkMode} setDarkModeNew={setDarkMode}/> <Outlet/></div>}>
-                        <Route index element={<Home/>}/>
-
+                        <Route index element={<div className="dark:bg-slate-500"><Home/> <Services/></div>}/>
+                        <Route path="/projects" element={<ProjectPage/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
                     </Route>
                 </Routes>
             </BrowserRouter>
