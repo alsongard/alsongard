4d3203c3d37212d41db683a80b40c38fce65738f
alsongard <alsongard@gmail.com>
2025-01-30 11:55:49 +0200

    projectData added

--- devportfolio/src/components/service.js
+++ devportfolio/src/components/service.js
@@ -1,8 +1,19 @@
 function Services()
 {
     return (
-        <section>
-            
+        <section className="mt-[10px] ">
+            <div className="w-[80%] p-[50px] mx-auto dark:bg-slate-500" >
+                <h1>Serivces I Offer</h1>
+                <ul className="list-disc text-left pl-[20px]">
+                    <li>Web Develoliment and design both front-end and back-end</li>
+                    <li>Data analyst</li>
+                    <li>Web Scraping</li>
+                    <li>Software Development using C++ and Python</li>
+                    <li>Building Prediction Models</li>
+                </ul>
+            </div>
         </section>
     )
 }
+
+export default Services;
