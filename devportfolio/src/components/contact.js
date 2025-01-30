b6b2813f8dcca6cec2aa55ebf2e6b809234a5dcb
alsongard <alsongard@gmail.com>
2025-01-30 14:28:06 +0200

    contact page setup

--- devportfolio/src/components/contact.js
+++ devportfolio/src/components/contact.js
@@ -2,7 +2,18 @@ function Contact()
 {
     return (
         <section>
-            
+            <form className="flex flex-col">
+                <label>Name:</label>
+                <input type='text' placeholder="Enter name..."/>
+                <label>Email:</label>
+                <input type='email' placeholder="Enter email..."/>
+                <label>Project:</label>
+                <textarea placeholder="Provide a description of the project "/>
+
+                <input type="submit" value="submit"/>
+            </form>
         </section>
     )
 }
+
+export default Contact;
