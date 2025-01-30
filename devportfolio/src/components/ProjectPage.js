599d81ee0262232bccd5e613d019235b1a2c48ef
alsongard <alsongard@gmail.com>
2025-01-30 12:06:18 +0200

    official project page for viewing different projects

---
+++ devportfolio/src/components/ProjectPage.js
@@ -0,0 +1,13 @@
+// import ProjectComponentView from "./projectView";
+import data from "./projectData";
+function ProjectPage()
+{
+    const myElement = Object.keys(data);
+    console.log(myElement)
+    return (
+        <section>
+            <h1>Project Page</h1>
+        </section>
+    )
+}
+export default ProjectPage;
