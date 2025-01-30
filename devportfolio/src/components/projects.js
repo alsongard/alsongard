c7183de56e16567ea37761ca63583c61fb3a9f8d
alsongard <alsongard@gmail.com>
2025-01-30 14:32:06 +0200

    projects section for homepage

--- devportfolio/src/components/projects.js
+++ devportfolio/src/components/projects.js
@@ -1,8 +1,12 @@
+import ProjectComponentView from "./projectComponentView";
+import data from "./projectData";
 function Projects()
 {
+    // const myElement = Object.keys(data);
+    // console.log(myElement)
     return (
         <section>
-
+            <h1>Projects</h1>
         </section>
     )
 }
