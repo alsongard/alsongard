319ba96f6f6e2eca72cac3808a66fee2e269e4a7
alsongard <alsongard@gmail.com>
2025-02-01 14:12:18 +0200

    footer content done

--- devportfolio/src/components/footer.js
+++ devportfolio/src/components/footer.js
@@ -1,8 +1,35 @@
+import { Link } from "react-router-dom";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 function Footer()
 {
     return (
-        <footer>
-            
+        <footer className="dark:bg-slate-600 py-[100px]">
+                <div className="text-center">
+                    <h2 className="text-[33px]" >AlsonGard</h2>
+                    <p>Days become seconds, weeks become minutes, months become hours and years just days.</p>
+                    <p>Pursuing every dream to become reality, building applications which aid in human activities and in a better world.</p>
+                </div>
+            <div className="flex mt-[40px] items-start justify-center gap-x-[200px] flex-row">
+                <div>
+                    <ul>
+                       <li><Link to="">Home</Link></li>
+                       <li><Link to="">Projects</Link></li>
+                       <li><Link to="">Services</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Contact</h1>
+                    <div className="flex justify-between w-[100px] flex-row">
+                        <Link to="/"><FaXTwitter/></Link>
+                        <Link to="/"><FaGithub/></Link>
+                        <Link to="/"><BiLogoGmail/></Link>
+
+                    </div>
+                </div>
+            </div>        
         </footer>
     )
 }
+export default Footer;
