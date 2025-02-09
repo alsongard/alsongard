fb33b5538e72b4627261078dd51481351501e295
alsongard <alsongard@gmail.com>
2025-02-09 16:03:33 +0200

    set the image prop for the projectcomponentview

--- devportfolio/src/components/projectComponentView.js
+++ devportfolio/src/components/projectComponentView.js
@@ -1,12 +1,12 @@
 function ProjectComponentView(prop)
 {
     return (
-        <div className="w-[33%]   px-[20px] rounded-md ">
+        <div className="w-[32%] h-[400px] shadow-[0px_0px_10px_lightblue] px-[20px] rounded-md ">
             <a className="cursor-pointer " target="_blank" href={prop.item.url}>
                 <div>
                     <h2>{prop.item.name}</h2>
                     <p>{prop.item.short_description}</p>
-                    <img src="" alt="project-image"/>
+                    <img className="w-[100%] rounded-md h-[300px] border-2 border-black " src={prop.item.img} alt="project-image"/>
                     <p>{}</p>
                 </div>
             </a>
