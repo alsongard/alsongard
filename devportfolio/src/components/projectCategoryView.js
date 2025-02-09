6cb4c1dc0d8d38f4a5bed144bf9d1e5f36bb2b9a
alsongard <alsongard@gmail.com>
2025-02-09 16:03:11 +0200

    set the image prop for the projectcomponentview

--- devportfolio/src/components/projectCategoryView.js
+++ devportfolio/src/components/projectCategoryView.js
@@ -8,7 +8,7 @@ function ProjectCategoryView(props)
     return (
         <div className="">
             <h1 className="font-semibold">{projectCategoryName}</h1>
-            <div className="border-2 border-[rgba(105,47,47,0.3)] dark:border-black  rounded-md pt-3 flex flex-row justify-between  items-center gap-y-4 flex-wrap px-[10px]">
+            <div className="border-2 border-[rgba(105,47,47,0.3)] dark:border-black  rounded-md py-3 flex flex-row justify-between  items-center gap-y-[20px] flex-wrap px-[10px]">
                 {projects.map(project => (
                         <ProjectComponentView key={project.id} item={project}/>
                     ))}
