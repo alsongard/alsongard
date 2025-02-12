5c94d16ef5d15d475d2b1a1dd16576030b3d929b
alsongard <alsongard@gmail.com>
2025-02-12 15:51:35 +0200

    setup styling for preloader

--- devportfolio/src/components/mains.js
+++ devportfolio/src/components/mains.js
@@ -7,24 +7,37 @@ import ProjectPage from "./ProjectPage";
 import Contact from "./contact";
 import TechStack from "./techStack";
 import Footer from "./footer";
+import Preloader from "./preloader";
 function Page()
 {
     const [darkMode, setDarkMode] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(true);
 
+    React.useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setIsLoading(false);
+        }, 5000)
+        return ()=>{clearTimeout(timer)}
+    },[])
     const answer = darkMode ? "dark" : ""
     return(
-        <main className={`${answer}`}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<div><Header darkModeNew={darkMode} setDarkModeNew={setDarkMode}/> <Outlet/> <Footer/> </div>}>
-                        <Route index element={<div className="dark:bg-slate-500"><Home/> <Services/> <TechStack/></div>}/>
-                        <Route path="/projects" element={<ProjectPage/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            
-        </main>
+        <div>
+            {isLoading ? (<Preloader/>) : (
+    
+            <main className={`${answer}`}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<div><Header darkModeNew={darkMode} setDarkModeNew={setDarkMode}/> <Outlet/> <Footer/> </div>}>
+                            <Route index element={<div className="dark:bg-slate-500"><Home/> <Services/> <TechStack/></div>}/>
+                            <Route path="/projects" element={<ProjectPage/>}/>
+                            <Route path="/contact" element={<Contact/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+                
+            </main>
+            )}
+        </div>
     )
 }
 export default Page;
