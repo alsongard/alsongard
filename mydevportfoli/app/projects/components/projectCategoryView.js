25b08c9ae532d63b8ff6062efb5043b3b0fcebfe
alsongard <alsongard@gmail.com>
2025-04-08 03:00:49 +0300

    project grid responsive set

--- mydevportfoli/app/projects/components/projectCategoryView.js
+++ mydevportfoli/app/projects/components/projectCategoryView.js
@@ -6,7 +6,7 @@ function ProjectCategoryView(props)
     return (
         <div className="mb-[50px]  mx-auto  rounded-md p-[10px]">
             <h1 className="font-semibold">{projectCategoryName}</h1>
-            <div className="grid grid-cols-3 gap-x-[30px] gap-y-[50px] shadow-[0px_0px_5px_#2e4053] rounded-md p-[30px] "> {/**dark:border-black  justify-items-stretch gap-x-[30px] border-[1px] border-black  items-center gap-y-[20px] flex-wrap px-[10px]*/}
+            <div className="grid grid-cols-3 max-[936px]:grid-cols-2  max-[732px]:grid-cols-1 gap-x-[30px] gap-y-[50px] shadow-[0px_0px_5px_#2e4053] rounded-md p-[30px] "> {/**dark:border-black  justify-items-stretch gap-x-[30px] border-[1px] border-black  items-center gap-y-[20px] flex-wrap px-[10px]*/}
                 {projects.map(project => (
                         <ProjectComponentView key={project.id} item={project}/>
                     ))}
