6b12750a2d2c888eef3beebb0a4898ed88afbdfb
alsongard <alsongard@gmail.com>
2025-04-20 22:45:14 +0300

    contact setup done

--- mydevportfoli/app/contact/page.tsx
+++ mydevportfoli/app/contact/page.tsx
@@ -1,7 +1,6 @@
 "use client"; // using use client as this form requires client(user) interaction, useState
 
 import React from "react";
-
 // export const metadata = {
 //     title: "Contact",
 //     description: "get in touch"
@@ -16,6 +15,7 @@ function Contact()
         phoneNumber: "",
         projectInfo: "",
     })
+
     function handleChange(event: React.ChangeEvent<HTMLInputElement>)
     {
         const {name, value} = event.target;
@@ -27,15 +27,14 @@ function Contact()
             )
         })
     }
-    function handleSubmit()
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>)
     {
-
 
     }
     console.log(formData);
     return (
         <section className=" py-[100px] dark:bg-slate-600">
-            <form onSubmit={handleSubmit} className="flex flex-col w-[50%] mx-auto  ">
+            <form onSubmit={handleSubmit} className="text-black flex flex-col w-[50%] mx-auto  ">
                 <label>Enter Name</label>
                 <input onChange={handleChange} value={formData.name} className="border-[2px] py-[1.5px] px-[3px] border-sky-300 rounded-md " type='text' placeholder="Name..."/>
                 <label className="mt-[9px]">Enter email</label>
@@ -43,7 +42,7 @@ function Contact()
                 <label className="mt-[9px]">Enter phone number</label>
                 <input  onChange={handleChange} value={formData.phoneNumber} className="border-[2px] py-[1.5px] px-[3px] border-sky-300 rounded-md" type='number' placeholder="Enter phonenumber..."/>
                 <label className="mt-[9px]">Enter Project Information</label>
-                <textarea   value={formData.projectInfo}  className="border-[2px] py-[1.5px] px-[3px] border-sky-300 rounded-md" placeholder="Provide a description of the project..."/>
+                <textarea  value={formData.projectInfo}  className="border-[2px] py-[1.5px] px-[3px] border-sky-300 rounded-md" placeholder="Provide a description of the project..."/>
 
                 <input className="bg-gradient-to-br text-white	from-[rgb(65,122,136)] to-[#06323a] w-[20%] mx-auto py-[2.5px] rounded-md my-[20px]" type="submit" value="submit"/>
             </form>
