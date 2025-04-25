1e9fa4dbe9bcf0758d9f60e944cc9b7db6e0ca4b
alsongard <alsongard@gmail.com>
2025-04-25 12:24:55 +0300

    landing page design done

--- mydevportfolio/app/page.tsx
+++ mydevportfolio/app/page.tsx
@@ -1,6 +1,11 @@
 import user_1 from "../public/images/user_1.jpg"
 import Link from "next/link";
 import Image from "next/image";
+import { FaTwitter, FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaLinkedin } from "react-icons/fa6";
+
 export const metadata = {
   title: "AlsonGard",
   description: "home page"
@@ -9,114 +14,85 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="">  
-      <section className="bg-gradient-to-tl from-[rgb(93,109,126)] to-[rgb(33,47,60)] h-[95vh]  ">
-        <div className="pt-[30px] text-white">
-          <h1 className="text-center text-[33px] font-bold">Gard Alson</h1>
-          <p className="text-center font-semibold">Full Stack Developer & PenTester</p>
+    <div className="w-[100vw]"> 
 
-          {/* short text for introduction */}
-          <p className="text-center w-[70%] mx-auto">I am a versatile Full Stack Developer and PenTester with a passion for building robust web applications and ensuring their security. With expertise in both front-end and back-end technologies, I strive to deliver seamless user experiences while safeguarding systems against vulnerabilities.</p>
-          
-          {/* icons for getting in touch */}
-          {/* <div className="flex  flex-row justify-between w-[250px] h-[45px] my-[20px] mx-auto">
-              <div className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950 "><FaGithub className="text-[23px]"/></div>
-              <div className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950"><FaLinkedin className="text-[23px]"/></div>
-              <div className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950"><FaTwitter className="text-[23px]"/></div>
-          </div> */}
+      {/* IMAGE CONTAINER */}
+      <div className="flex flex-row  items-start justify-evenly gap-x-[30px] my-[50px] w-[90%] mx-auto  py-[70px]  ">
+        <div className="">
+          <Image className="rounded-[100%]" width={450}  height={550} src="/images/anime_2.jpg" alt=""/>
+        </div>
 
-          <div className="w-[330px] myImage mx-auto mt-[20px] h-[330px]">
-              <Image  width={330} height={330} className=" w-[100%] h-[100%] rounded-full " src="/images/user_1.jpg" alt="fullstack/hacker"/>
+        <div className='pt-[50px] pl-[5px] text-white '>
+          <p className='text-[20px]'><span className='text-[60px] pr-0'>H</span>i there <span className="dot"></span> <span className="dot"></span> <span className="dot"></span> </p>
+          <p className='text-[20px] capitalize'>am Gard Alson</p>
+          <p className="text-[20px]">Welcome to my Porfolio.  </p>
+          <p className='text-[20px]'>I'm a Full Stack Developer, PenTester, Game Developer and Data Analyst </p>
+
+          <h2 className=" text-center text-[20px] mt-[50px] mb-[5px]" >Get in Touch</h2>
+          {/* icons */}
+          <div className='flex flex-row justify-evenly border-2 mx-auto py-[10px]  w-1/2 border-white     rounded-[50px] '>
+            
+            <div className='h-[30px] w-[30px]  rounded-[100%] hover:scale-[1.4]  ease-in-out'>
+              <a href="http://github.com/alsongard" target="_blank">
+                <FaGithub className='h-[100%] w-[100%] rounded-[100%] hover:text-black text-white bg-sky-500'/>
+                {/* <FaGithub className='h-[100%] w-[100%] rounded-[100%] hover:bg-black text-blue  bg-slate-400  '/> */}
+              </a>
+            </div>
+
+            <div className='h-[30px] w-[30px]  rounded-[100%] hover:scale-[1.4] h ease-in-out'>
+              <a href="mailto:alsongadizo@gmail.com" target="_blank">
+                <BiLogoGmail className='h-[100%] w-[100%] rounded-[100%] hover:text-black text-white bg-sky-500'/>
+              </a>
+            </div>
+
+            <div className='h-[30px] w-[30px]  rounded-[100%] hover:scale-[1.4] hover:black  ease-in-out'>
+              <a href="https://x.com/alsongadizo" target="_blank">
+                <FaTwitter className='h-[100%] w-[100%] rounded-[100%] hover:text-black text-white bg-sky-500'/>
+              </a>
+            </div>
+
           </div>
+
+        </div>
+
+      </div>
+
+        {/* ABOUT ME */}
+      <section className='text-white  my-[50px]'>
+        <h1 className='text-center text-[23px] font-bold mt-[50px] mb-[10px]'>About Me</h1>
+        <div className="px-[50px] text-center">
+          <p className='text-[20px]'>I’m a curious and creative developer who loves building cool stuff with code. From designing sleek front-ends and developing powerful back-ends, to diving into cybersecurity as a pentester, creating games, and even exploring the world of data analysis—I enjoy wearing multiple hats and learning along the way. I'm all about solving problems, experimenting with new tech, and turning ideas into real, working things people can use and enjoy.Let’s build something awesome!</p>
+        </div>
+        <div className='text-center my-[20px]'>
+          <p className='text-[20px]'>To know more About Me click the button below</p>
+          <button className='my-[10px] bg-sky-500 py-[3.5px] px-[10px] rounded-md text-[20px] hover:text-black hover:scale-[1.1]'><Link href='/'>About Me</Link></button>
         </div>
       </section>
-      <section className="dark:bg-slate-600 pt-[10px] ">
 
+      {/* SERVICES */}
+      <section className="text-white  my-[50px] ">
         {/* <div className=" w-[80%] text-white p-[50px] mx-auto dark:bg-slate-500" > */}
+        <h1 className='text-center text-[23px] font-bold mt-[50px] mb-[10px]'>Services</h1>
         <div className=" w-[80%] dark:text-white p-[50px] mx-auto " >
-          <h1>Serivces I Offer</h1>
+          <h1 className='text-[20px]'>Serivces I Offer</h1>
           <ul className="list-disc text-left pl-[20px]">
-            <li>Web Develoliment and design both front-end and back-end</li>
-            <li>Data analyst</li>
-            <li>Web Scraping</li>
-            <li>Software Development using C++ and Python</li>
-            <li>Building Prediction Models</li>
+            <li className='text-[20px]'>Web Development and design both front-end and back-end</li>
+            <li className='text-[20px]'>Data analyst</li>
+            <li className='text-[20px]'>Web Scraping</li>
+            <li className='text-[20px]'>Software Development using C++ and Python</li>
+            <li className='text-[20px]'>Game Development</li>
           </ul>
         </div>
-      </section>
-      <section className="dark:bg-slate-600 pt-[20px] pb-[50px]">
-        <h1 className="text-white text-center text-[40px] font-semibold">Techstack</h1>
-
-        {/* techstack container */}
-        <div className=" mx-auto w-[100%] grid grid-cols-3 gap-x-4 px-[20px] max-[778px]:grid-cols-2 max-[500px]:grid-cols-1 text-[#5d6d7e]">
-          <div className="techstack-container  dark:border-none border-2 border-black text-center mt-[30px] bg-white py-[10px] px-[10px] rounded-md ">
-            <h1 className="text-[23px]">Front-End Developer</h1>
-            <p className="">Crafting visually appealing and responsive landing pages and user interfaces to enhance user experience.</p>
-            <h2 className="mt-[10px]">Core Technologies</h2>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript (ES6+)</li>
-            </ul>
-            <h2 className="mt-[10px]">Frameworks & Libraries</h2>
-            <ul>
-                <li>React</li>
-                <li>Tailwindcss</li>
-            </ul>
-          </div>
-
-          <div className="techstack-container border-2 border-black text-center mt-[30px] bg-white py-[10px] px-[10px] rounded-md ">
-          
-            <h1 className="text-[23px]" >Back-End Developer</h1>
-            <p>Building and maintaining robust server-side applications and databases to ensure smooth data flow and secure storage.</p>
-
-            <h2 className="mt-[10px]">Core Technologies</h2>
-            <ul>
-                <li>PHP</li>
-                <li>SQL (MySQL)</li>
-            </ul>
-            
-            <h2 className="mt-[10px]" >Frameworks & Tools</h2>
-            <ul>
-                <li>Node.js</li>
-                <li>Express.js</li>
-            </ul>
-          </div>  
-
-          <div className="techstack-container border-2 border-black text-center mt-[30px] bg-white py-[10px] px-[10px] rounded-md ">
-          
-            <h1 className="text-[23px]">Data Analyst</h1>
-            <p>Transforming raw data into actionable insights to drive better decision-making through analysis and visualization.</p>
-            
-            <h2 className="mt-[10px]">Core Technologies</h2>
-            <ul>
-                <li>Python</li>
-            </ul>
-
-            <h2 className="mt-[10px]">Libraries</h2>
-            <ul>
-                <li>Pandas</li>
-                <li>Matplotlib</li>
-                <li>Scikit learn</li>
-                <li>Numpy</li>
-                <li>Streamlit</li>
-            </ul>
-          </div>
-
-          <div className="techstack-container border-2 border-black text-center mt-[30px] bg-white py-[10px] rounded-md ">
-          
-            <h1 className="text-[23px]">Software Development</h1>
-            <p>Designing and developing high-performance applications using C++ for both desktop and embedded systems</p>
-
-            <h2>TechStack</h2>
-            <ul>
-                <li>C++</li>
-            </ul>
-          </div>
+        <div className='text-center my-[20px]'>
+          <p className='text-[20px]'>Click below To</p>
+          <button className='my-[10px] bg-sky-500 py-[3.5px] px-[10px] rounded-md text-[20px] hover:text-black hover:scale-[1.1]'><Link href='/'>Hire Me</Link></button>
         </div>
+      </section>
 
-        <Link href="/projects"><button className="mt-[50px] px-[10px] py-[2px] border-2 border-black rounded-md text-[#5d6d7e] block  mx-auto bg-white">View Projects</button></Link>
-        </section>
+
+
+
+
     </div>
 
     );
