1fe3c80d6254d42ee8474174cd108dcb2c2e2a3d
alsongard <alsongard@gmail.com>
2025-04-30 23:55:35 +0300

    indent  header code

--- mydevportfolio/app/components/header.tsx
+++ mydevportfolio/app/components/header.tsx
@@ -70,85 +70,85 @@ export default function Header()
 
 
     return (
-        <header className='flex bg-sky-500 w-[90%] mx-auto mt-[80px] py-[22px] px-[20px] rounded-[25px] item-center justify-between'>
-                  <div>
-                    <h1 className="pl-[15px] text-[20px]">AlsonGard</h1>
-                  </div>
-        
-                  <div className="pr-[15px]">
-                    <ul className='flex text-[20px] justify-between  w-[500px] flex-row items-center'>
-                      <li>
-                        <Link
-                          className="hover:text-white" 
-                          href="/"
-                        >
-                          Portfolio
-                        </Link>
-                      </li>
-                      <li>
-                        <Link 
-                          className="hover:text-white" 
-                          href="/projects"
-                          >
-                            Projects
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          className="hover:text-white" 
-                          href="/contact"
-                        >
-                          Contact
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          className="hover:text-white" 
-                          href="/about"
-                        >
-                          About
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
+      <header className='flex bg-sky-500 w-[90%] mx-auto mt-[80px] py-[22px] px-[20px] rounded-[25px] item-center justify-between'>
+        <div>
+          <h1 className="pl-[15px] text-[20px]">AlsonGard</h1>
+        </div>
 
-                  {/* small menu  */}
-            {/* <div className={clsx(smallVisible ? "block " : "hidden" , "smallerMenu min-[530px]:hidden  absolute right-[50px] top-[50px] bg-[#475569]  rounded-md flex flex-col py-[20px]  items-center" )}> */}
-            <div className={clsx(smallVisible ? "block " : "hidden" , "smallerMenu min-[530px]:hidden  absolute right-[50px] top-[50px] bg-[#475569] w-[150px] rounded-md flex flex-col py-[20px]  items-center" )}>
-                <ul className="list-none">
-                    <li>
-                        <Link
-                            className="hover:text-black" 
-                            href="/"
-                        >
-                            Portfolio
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className="hover:text-black" 
-                            href="/projects"
-                        >
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className="hover:text-black" 
-                            href="/contact"
-                        >
-                            Contact
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/about"
-                        >
-                        About
-                    </Link>
-                    </li>
-                </ul>
-            </div>
+        <div className="pr-[15px]">
+          <ul className='flex text-[20px] justify-between  w-[500px] flex-row items-center'>
+            <li>
+              <Link
+                className="hover:text-white" 
+                href="/"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link 
+                className="hover:text-white" 
+                href="/projects"
+                >
+                  Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:text-white" 
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:text-white" 
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* small menu  */}
+        {/* <div className={clsx(smallVisible ? "block " : "hidden" , "smallerMenu min-[530px]:hidden  absolute right-[50px] top-[50px] bg-[#475569]  rounded-md flex flex-col py-[20px]  items-center" )}> */}
+        <div className={clsx(smallVisible ? "block " : "hidden" , "smallerMenu min-[530px]:hidden  absolute right-[50px] top-[50px] bg-[#475569] w-[150px] rounded-md flex flex-col py-[20px]  items-center" )}>
+          <ul className="list-none">
+            <li>
+                <Link
+                    className="hover:text-black" 
+                    href="/"
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li>
+                <Link 
+                    className="hover:text-black" 
+                    href="/projects"
+                >
+                    Projects
+                </Link>
+            </li>
+            <li>
+                <Link
+                    className="hover:text-black" 
+                    href="/contact"
+                >
+                    Contact
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/about"
+                >
+                About
+            </Link>
+            </li>
+          </ul>
+        </div>
 
     </header>
     )
