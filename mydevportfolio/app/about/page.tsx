b9e281d624664867700644b7c16b386d287f5a3c
alsongard <alsongard@gmail.com>
2025-05-03 15:22:50 +0300

    about page responsive nearly done:laptops,tablets done

--- mydevportfolio/app/about/page.tsx
+++ mydevportfolio/app/about/page.tsx
@@ -6,11 +6,11 @@ import { BiLogoGmail } from "react-icons/bi";
 
 export default function About() {
   return (
-    <section className="text-white bg-black w-[100vw]">
+    <section className="bg-white text-black dark:text-white dark:bg-black w-[100vw]">
       <h1 className="text-center text-[40px] font-bold pt-[30px]">About Me</h1>
       {/* home section */}
-      <div className='w-[80%] mx-auto text-[20px] my-[10px]'>
-        <p>👋 About Me Hey, I’m [Your Name] — a developer with a passion for building, breaking, and understanding how things work behind the screen.
+      <div className='w-[80%] mx-auto  my-[10px]'>
+        <p className='max-md:text-[17px] text-[20px]'>👋 About Me Hey, I’m Gard Alson — a developer with a passion for building, breaking, and understanding how things work behind the screen.
           My journey into tech didn’t start with a textbook or a tutorial — it started with curiosity.
           I’ve always loved tinkering, whether it’s figuring out how to design a beautiful front-end, optimizing a back-end system, or diving into networks and systems as a pentester just to see what’s possible (and what shouldn't be 👀).
           I build projects that excite me, challenge me, and push me to grow. Some days I’m deep into writing clean UI components, other days I’m solving logic-heavy backend tasks or cracking open cybersecurity challenges.
@@ -20,32 +20,28 @@ export default function About() {
       </div>
       <div className="w-[100%] my-[100px]">
         <h1 className='text-[30px] text-center font-semibold py-[10px]'>Skills</h1>
-        <div className='flex flex-col space-y-[80px] h-[400px] py-[20px] overflow-x-auto items-center  text-white w-[50%] mx-auto  border-2 border-white rounded-md'>
-          <section className='flex flex-row gap-x-[30px]  justify-center items-start w-[90%] mt-[20px]'>
-            <div className='intro  sticky top-0 w-1/4 '>
+        <div className='flex flex-col space-y-[80px] h-[400px] py-[20px] overflow-y-auto items-center  text-white w-[80%] mx-auto  border-2 dark:border-white border-black rounded-md'>
+          <section className='flex flex-row max-sm:flex-col max-sm:gap-y-[10px] max-sm:items-center  items-start w-[80%] justify-between mt-[20px]'>
+            <div className='intro w-[200px] flex-wrap sticky top-0'>
               <h1 className='text-[22px] bg-slate-800 py-[10px] px-[5px] rounded-md'>Front-end Development</h1>
             </div>
-            <div className='shadow-[0px_0px_5px_black] bg-slate-800 w-3/4 p-[20px] rounded-md '>
-              <ul className='grid grid-cols-2 justify-items-center   gap-y-[50px]' >
+            <div className='shadow-[0px_0px_5px_black]  w-[50%]  bg-slate-800 p-[20px] rounded-md '>
+              {/* <ul className='grid grid-cols-2 justify-items-center  gap-y-[50px]' > */}
+              <ul className='flex flex-row gap-x-[30px] items-center justify-start max-[908px]:justify-center pl-[17px] flex-wrap w-[100%]  gap-y-[50px]' >
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" /></li>
-                {/* <li className='text-[22px]' >React</li>
-                <li className='text-[22px]' >NextJS</li>
-                <li className='text-[22px]' >TailwindCSS5</li>
-                <li className='text-[22px]' >HTML5</li>
-                <li className='text-[22px]' >Javascript</li> */}
               </ul>
             </div>
           </section>
 
-          <section className='flex flex-row gap-x-[30px]  justify-center items-start w-[90%] mt-[20px]'>
-            <div className='intro sticky top-0 w-1/4'>
+          <section className='flex flex-row max-sm:flex-col max-sm:gap-y-[10px] max-sm:items-center  items-start w-[80%] justify-between mt-[20px]'>
+            <div className='intro w-[200px] flex-wrap sticky top-0'>
               <h1 className='text-[22px] bg-slate-800 py-[10px] px-[5px] rounded-md'>Back-End Development</h1>
             </div>
-            <div className='shadow-[0px_0px_5px_black] w-3/4 bg-slate-800  p-[20px] rounded-md '>
-              <ul className='grid grid-cols-2 justify-items-center gap-y-[50px]'>
+            <div className='shadow-[0px_0px_5px_black]  w-[50%]  bg-slate-800 p-[20px] rounded-md '>
+              <ul className='flex flex-row gap-x-[30px] items-center justify-start max-[908px]:justify-center pl-[17px] flex-wrap w-[100%]  gap-y-[50px]'>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" /></li>
@@ -55,35 +51,35 @@ export default function About() {
             </div>
           </section>
 
-          <section className='flex flex-row gap-x-[30px]  justify-center items-start w-[90%] mt-[20px]'>
-            <div className='intro sticky top-0 w-1/4'>
+          <section className='flex flex-row max-sm:flex-col max-sm:gap-y-[10px] max-sm:items-center  items-start w-[80%] justify-between mt-[20px]'>
+            <div className='intro w-[200px] flex-wrap sticky top-0'>
               <h1 className='text-[22px] bg-slate-800 py-[10px] px-[5px] rounded-md'>Penetration Testing</h1>
             </div>
-            <div className=' shadow-[0px_0px_5px_black] bg-slate-800 w-3/4 p-[20px] rounded-md '>
-              <ul className=' grid grid-cols-2  justify-items-center  px-[10px]  gap-y-[50px]'>
+            <div className='shadow-[0px_0px_5px_black]  w-[50%]  bg-slate-800 p-[20px] rounded-md '>
+              <ul className='flex flex-row gap-x-[30px] items-center justify-start max-[908px]:justify-center pl-[17px] flex-wrap w-[100%]  gap-y-[50px]'>
                 <img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" />
               </ul>
             </div>
           </section>
 
-          <section className='flex flex-row gap-x-[30px]  justify-center items-start w-[90%] mt-[20px]'>            
-            <div className='intro sticky top-0 w-1/4'>
+          <section className='flex flex-row max-sm:flex-col max-sm:gap-y-[10px] max-sm:items-center  items-start w-[80%] justify-between mt-[20px]'>            
+            <div className='intro w-[200px] flex-wrap sticky top-0'>
               <h1 className='text-[22px] bg-slate-800 py-[10px] px-[5px] rounded-md'>Game Development</h1>
             </div>
-            <div className='shadow-[0px_0px_5px_black] bg-slate-800 w-3/4 p-[20px] rounded-md '>
-              <ul className='grid  grid-cols-2  justify-items-center  px-[10px]  gap-y-[50px]'>
+            <div className='shadow-[0px_0px_5px_black]  w-[50%]  bg-slate-800 p-[20px] rounded-md'>
+              <ul className='flex flex-row gap-x-[30px] items-center justify-start max-[908px]:justify-center pl-[17px] flex-wrap w-[100%]  gap-y-[50px]'>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-plain-wordmark.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]'src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" /></li>
               </ul>
             </div>
           </section>
 
-          <section className='flex flex-row gap-x-[30px]   justify-center items-start w-[90%] mt-[20px]'>
-            <div className='intro sticky top-0 w-1/4'>
+          <section className='flex flex-row max-sm:flex-col max-sm:gap-y-[10px] max-sm:items-center  items-start w-[80%] justify-between mt-[20px]'>
+            <div className='intro w-[200px] flex-wrap sticky top-0'>
               <h1 className='text-[22px] bg-slate-800 py-[10px] px-[5px] rounded-md'>Data Analysis </h1>
             </div>
-            <div className='shadow-[0px_0px_5px_black] bg-slate-800 w-3/4 p-[20px] rounded-md '>
-              <ul className=' grid grid-cols-2  justify-items-center  px-[10px] gap-y-[50px]'>
+            <div className='shadow-[0px_0px_5px_black]  w-[50%]  bg-slate-800 p-[20px] rounded-md'>
+              <ul className='flex flex-row gap-x-[30px] items-center justify-start max-[908px]:justify-center pl-[17px] flex-wrap w-[100%]  gap-y-[50px]'>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg" /></li>
                 <li><img className='w-[100px] h-[100px] rounded-md border-2 border-white tech-icon p-[10px] hover:scale-[1.1]' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original-wordmark.svg" /></li>
