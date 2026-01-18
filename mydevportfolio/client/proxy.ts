14f0068dea014ca0304fde3b9d1ab99350e0798d
alsongard <alsongard@gmail.com>
2026-01-18 17:08:32 +0200

    proxy error fixec

--- mydevportfolio/client/proxy.ts
+++ mydevportfolio/client/proxy.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import {authConfig} from "./auth.config"
 
 
-export default NextAuth(authConfig).auth:
+export default NextAuth(authConfig).auth;
 export const config = {
     // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
