c35f7e5cab7d30a1b4ea4a626f0f53930f2caa56
alsongard <alsongard@gmail.com>
2026-02-20 07:24:05 +0200

    added types

--- mydevportfolio/client/type.d.ts
+++ mydevportfolio/client/type.d.ts
@@ -1,11 +1,15 @@
 
-type projectData =  {
-    string: {
-        id: number,
-        name: string,
-        short_description: string,
-        description: string,
-        img: string,
-        url: string
-    }
+export type Data =  {
+   [key:string]:ProjectData[]
 }
+export interface ProjectData {
+    id: number,
+    name: string,
+    short_description: string,
+    date: string,
+    description: string,
+    img: string,
+    url: string,
+    techStack: string[],
+}
+
