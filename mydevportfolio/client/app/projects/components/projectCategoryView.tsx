c7fe70b636614c268de7b3f9c79e7a8582e39f88
alsongard <alsongard@gmail.com>
2026-02-27 23:30:34 +0200

    update: responsiveness set for projects page

--- mydevportfolio/client/app/projects/components/projectCategoryView.tsx
+++ mydevportfolio/client/app/projects/components/projectCategoryView.tsx
@@ -14,6 +14,7 @@ type CategoryProps = {
     projectCategoryName: string,
     projects : projectData[]
 }
+// max-[936px]:grid-cols-2  max-[732px]:grid-cols-1
 function ProjectCategoryView(props: CategoryProps)
 {
     const {projectCategoryName, projects} = props;
@@ -21,7 +22,7 @@ function ProjectCategoryView(props: CategoryProps)
     return (
         <div className="mb-[50px]  mx-auto  rounded-md p-[10px]">
             <h1 className="font-semibold">{projectCategoryName.toUpperCase()}</h1>
-            <div className="grid grid-cols-1 max-[936px]:grid-cols-2  max-[732px]:grid-cols-1 gap-x-[30px] gap-y-[50px] shadow-[0px_0px_5px_#2e4053] rounded-md p-[30px] "> {/**dark:border-black  justify-items-stretch gap-x-[30px] border-[1px] border-black  items-center gap-y-[20px] flex-wrap px-[10px]*/}
+            <div className="grid grid-cols-1  gap-x-[30px] gap-y-[50px] shadow-[0px_0px_5px_#2e4053] rounded-md p-[30px] "> {/**dark:border-black  justify-items-stretch gap-x-[30px] border-[1px] border-black  items-center gap-y-[20px] flex-wrap px-[10px]*/}
                 {projects.map(project => (
                         <ProjectComponentView key={project.id} item={project}/>
                     ))}
--- mydevportfolio/client/app/projects/components/projectComponentView.tsx
+++ mydevportfolio/client/app/projects/components/projectComponentView.tsx
@@ -17,21 +17,20 @@ interface PropTypes {
 function ProjectComponentView(prop:PropTypes)
 {
     return (
-        <div className=" py-[10px] flex flex-row  space-x-[30px] border-white relative h-[300px] shadow-[0px_0px_10px_black] px-[20px]  rounded-md ">
+        <div className=" py-[10px] flex flex-row max-slg:flex-col max-slg:h-auto  max-slg:gap-y-[20px]  space-x-[30px] border-white relative h-[300px] shadow-[0px_0px_10px_black] px-[20px]  rounded-md ">
             <a className="cursor-pointer" target="_blank" href={prop.item.url}>
-                <div className=''> 
-                    <div className="relative h-[270px] w-[350px] rounded-md ">
-                        <Image fill={true} className="rounded-md " src={prop.item.img} alt="project-image"/> 
+                <div className='relative'> 
+                    <div className="relative h-[270px] w-[350px] max-sm:h-[200px] max-sm:w-full rounded-md ">
+                        <Image fill={true} className="rounded-md" src={prop.item.img} alt="project-image"/> 
                     </div>
                 </div>
             </a>
             <div>
                 <h2 className='font-bold pb-[10px]'>{prop.item.name}</h2>
                 <p>{prop.item.description}</p>
-                <div className='flex flex-row space-x-[10px] py-[10px] '>
+                <div className='flex flex-row flex-wrap gap-y-2.5 space-x-[10px] py-[10px] '>
                     {
                         prop.item.techStack.map((item, index:number)=>
-                        
                             <p key={index} className="py-[5px] px-[10px] bg-[#291263] rounded-md">{item}</p>
                         )
                     }
