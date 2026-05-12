3ef9d6c8302cb66cb032930071c052465f99f3cb
alsongard <alsongard@gmail.com>
2026-05-12 18:28:02 -0400

    added remotePatterns for images

--- mydevportfolio/client/next.config.mjs
+++ mydevportfolio/client/next.config.mjs
@@ -1,3 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
@@ -7,7 +14,19 @@ const nextConfig = {
         // !! WARN !!
         ignoreBuildErrors: false,
     },
-
+    outputFileTracingRoot: path.join(__dirname),
+    serverComponentsExternalPackages: ['pg'],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/dzth2gguw/image/upload/**',
+            },
+        ],
+    }
 };
+
+
 export default nextConfig;
 
