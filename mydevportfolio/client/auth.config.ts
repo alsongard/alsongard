43f6ef5c4199510a9ab51b1506acd3c748c56d76
alsongard <alsongard@gmail.com>
2026-05-20 18:31:39 -0400

    configuring nextauth using credentials,zod for typechecking input

---
+++ mydevportfolio/client/auth.config.ts
@@ -0,0 +1,38 @@
+import { log } from "console";
+import { request } from "http";
+import next from "next";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const authConfig = {
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        authorized({auth, request: {nextUrl}})
+        {
+            // console.log(`this is auth`);
+            // log(auth);
+
+            // console.log(`this is nextURL:`);
+            // log(nextUrl);
+
+
+            const isLoggedIn = !!auth?.user; // if auth exist, check user: and if user exists: true [negate twice to get back true (true --> false --> true)]
+            const isOnDashBoard = nextUrl.pathname.startsWith("/dashboard");
+            // console.log(`isLogged In : ${isLoggedIn}`);
+            // console.log(`isOnDashboard: ${isOnDashBoard}`);
+            if (isOnDashBoard)
+            {
+                if (isLoggedIn) return true; // if isLoggedIn returns and exits
+                return false; // otherwise false : redirected to signIn page: /login
+            }
+            else if (isLoggedIn) // isLoggedIn is true
+            {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return false;
+        }
+    },
+    providers : [Credentials],
+} satisfies NextAuthConfig;
---
+++ mydevportfolio/client/auth.ts
@@ -0,0 +1,64 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import {z} from "zod";
+import { pool } from "./lib/db";
+import bcrypt from 'bcrypt';
+import { log } from "console";
+
+async function getUser(email:string)
+{
+    // log(`We are in getUser: ${email}`);
+    try
+    {
+        const data = await pool.query(`SELECT * FROM "users" WHERE email = $1;`, [email])
+        return data.rows[0]; // select thhe first object in the array of data.rows: [{}]
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        throw new Error('Failed to fetch user.')
+    }
+}
+
+export const {auth, signIn, signOut} = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                // console.log('in auth.ts on authorized() function');
+                // console.log("credentials");
+                console.log(credentials);
+                const parsedCredentials  = z // zod is used to check if email and password are given | the format is correct: our case string
+                    .object({ email: z.email(), password: z.string().min(6) })
+                    .safeParse({
+                        email: credentials.username,
+                        password: credentials.password,
+                    });
+                // log(`this is parsedCredentials`);
+                // log(parsedCredentials);
+
+                if (parsedCredentials.success)
+                {
+                    const {email, password} = parsedCredentials.data;
+                    // log(`In if condition for parsedCredentials:`);
+                    // log(`email:${email}\npassword:${password}`)
+                    const user = await getUser(email);
+                    // console.log(`user: `);
+                    // console.log(user);
+                    if (!user) return null; // defensive programming
+                    const passwordMatch = await bcrypt.compare(password, user.userpassword); // compares the password
+                    // console.log(`passwordMatch: ${passwordMatch}`);
+                    if (passwordMatch) return user; 
+                }
+                else 
+                {
+                    log("Did not pass : zod credentials");
+
+                }
+                return null; // if parsed credentials
+
+            }
+        })
+    ]
+})
