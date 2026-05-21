f0ab37718ff283653e2e84451d502f5fffd1d1c8
alsongard <alsongard@gmail.com>
2026-05-21 02:29:46 -0400

    login page working: integrated with LoginForm:uses server action for loggin

---
+++ mydevportfolio/client/app/(public)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/app/components/loginComponent";
+
+export default function SignIn()
+{
+    return (
+        <div className="mt-[100px]">
+            <LoginForm/>
+        </div>
+    )
+}
