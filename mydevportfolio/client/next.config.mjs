3821d351cbd386b56dfd92aa757dc3c3f7a1c5d6
alsongard <alsongard@gmail.com>
2026-05-22 00:49:26 -0400

    update for hosting

--- mydevportfolio/client/next.config.mjs
+++ mydevportfolio/client/next.config.mjs
@@ -5,6 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+// console.log(`__filename: ${__filename}`);
+// console.log(`__dirname: ${__dirname}`);
+// console.log("Path.join" + path.join(__dirname));
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
