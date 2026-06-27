0c1a0830899373784944953e193a41d4ddb38d2a
alsongard <alsongard@gmail.com>
2026-06-26 20:47:25 -0400

    style: hover text color change

--- mydevportfolio/client/app/components/footer.tsx
+++ mydevportfolio/client/app/components/footer.tsx
@@ -1,8 +1,8 @@
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
-import React from 'react'
 import Link from "next/link";
+
 export default function Footer() {
   return (
     // <footer className="bg-gradient-to-tl from-[rgb(93,109,126)] to-[rgb(33,47,60)] py-[100px] dark:bg-black dark:text-white">
@@ -24,9 +24,9 @@ export default function Footer() {
             <div>
             <h1 className="text-center">Contact</h1>
                 <div className="flex flex-row justify-between w-[200px]  items-center flex-wrap max-[510px]:h-[auto] max-[510px]:w-[50px] max-[510px]:flex-col max-[510px]:gap-y-[10px] ">
-                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950" href="https://x.com/alsongadizo"><FaXTwitter/></a>
-                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950" href="http://github.com/alsongard"><FaGithub/></a>
-                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:bg-white hover:border-2 hover:border-slate-950" href="mailto:alsongadizo@gmail.com"><BiLogoGmail/></a>
+                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:text-black hover:bg-white hover:border-2 hover:border-slate-950" href="https://x.com/alsongadizo"><FaXTwitter/></a>
+                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:text-black hover:bg-white hover:border-2 hover:border-slate-950" href="http://github.com/alsongard"><FaGithub/></a>
+                    <a target="_blank" className="bg-slate-500 p-[10px] rounded-md hover:text-black hover:bg-white hover:border-2 hover:border-slate-950" href="mailto:alsongadizo@gmail.com"><BiLogoGmail/></a>
                 </div>
             </div>
         </div>        
